e62c06747209cbfa93ae0f24a3e6dc86bcd7dc44
Dionuta <diounta@gmail.com>
2019-02-27 20:16:24 -0800

    MVP Done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+const CardBanner = () =>{
+    return (
+        <div className='card'>
+          <img src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'  />
+          <CardContent />
+        </div>
+    )
+} 
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return (
+        <div className="container">
+             <h4> Get Started With React </h4> 
+             <p>React makes it painless to create interactive UIs. Design simple views for 
+            each state in your application.</p>  
+            <span>reactjs.org</span> 
+        </div>
+    )
+}
+
+export default CardContent;
