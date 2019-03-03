28ebdb50e5503f82dee6e1369f1049f932203759
Dionuta <diounta@gmail.com>
2019-03-03 03:38:41 -0800

    stretch done

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
-
+import * as math from 'mathjs';
 
 
 class App extends Component {
@@ -19,17 +19,20 @@ class App extends Component {
     this.setState({input: this.state.input + val }) //allows me to update the input
   }
 
+  handelEqual = () => {
+    this.setState({input:math.eval(this.state.input)})
+  }
  
   render(){
   return (
     <div className= 'outerdiv'>
       <CalculatorDisplay input={this.state.input} ></CalculatorDisplay>
       <ActionButton handleClear={() => this.setState({ input: '' })} className='clear'text='clear' />
-      <NumberButton handleClick={this.addToInput} className='symbol' text="&divide;" />
+      <NumberButton handleClick={this.addToInput} className='symbol' text="/" />
       <NumberButton handleClick={this.addToInput} className='number' text="7" />
       <NumberButton handleClick={this.addToInput} className='number' text="8" />
       <NumberButton handleClick={this.addToInput} className='number' text="9" />
-      <NumberButton handleClick={this.addToInput} className='symbol' text="&times;" />
+      <NumberButton handleClick={this.addToInput} className='symbol' text="*" />
       <NumberButton handleClick={this.addToInput} className='number' text="4" />
       <NumberButton handleClick={this.addToInput} className='number' text="5" />
       <NumberButton handleClick={this.addToInput} className='number' text="6" />
@@ -39,7 +42,7 @@ class App extends Component {
       <NumberButton handleClick={this.addToInput} className='number' text="3" />
       <NumberButton handleClick={this.addToInput} className='symbol' text="+" />
       <NumberButton handleClick={this.addToInput} className='zero'text='0' />
-      <NumberButton handleClick={this.addToInput} className='symbol' text="=" />
+      <NumberButton handleClick={() => this.handelEqual()} className='symbol' text="=" />
     </div>
   );
  };
